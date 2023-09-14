cb61c1f448b070a19c1b87e2f46186d63df98cfd
Alvaro Mello <alvaromello@prefeitura.sp.gov.br>
2023-09-14 13:31:29 -0300

    Primeira versão estável e com testes.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::env;
 
 use chiisana_url::{param_reader, properties_reader, server};
+use chiisana_url::properties_reader::STATIC_CONFIG;
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-
     let config_file = param_reader::read_params(args);
-
     properties_reader::initialize_config(config_file);
+    println!("");
+    println!("               小                          さ                       な  ");
+    println!("               _____                       _____                    _____  ");
+    println!("             / \\    \\                   / \\     \\                  /\\    \\");
+    println!("            /:: \\____\\                 /:: \\     \\                /::\\____\\");
+    println!("           /:::/    /                 /:::: \\     \\              /:::/    /");
+    println!("          /:::/    /                 /:::::: \\     \\            /:::/    /");
+    println!("         /:::/    /                 /:::/ \\:::\\     \\          /:::/    /");
+    println!("        /:::/    /                 /:::/__ \\:::\\     \\        /:::/    /");
+    println!("       /:::/    /                 /::::\\    \\:::\\     \\      /:::/    /");
+    println!("      /:::/    /        _____    /::::::\\    \\:::\\     \\    /:::/    /");
+    println!("     /:::/____/       / \\    \\  /:::/\\:::\\    \\:::\\_____\\  /:::/    /");
+    println!("     |:::|   /       /:: \\____\\/:::/  \\:::\\    \\:::|    | /:::/____/");
+    println!("     |:::|____\\     /:::/    / \\::/   |::::\\  /::: |____| \\:::\\    \\");
+    println!("      \\:::\\    \\   /:::/    /   \\/____|:::::\\/::::/    /   \\:::\\    \\");
+    println!("       \\:::\\    \\ /:::/    /          |::::::::::/    /     \\:::\\    \\");
+    println!("        \\:::\\    /:::/    /           |::| \\::::/    /       \\:::\\    \\");
+    println!("         \\:::\\__/:::/    /            |::|  \\::/____/         \\:::\\    \\");
+    println!("          \\::::::::/    /             |::|   ~|                \\:::\\    \\");
+    println!("           \\::::::/    /              |::|    |                 \\:::\\    \\");
+    println!("            \\::::/    /               \\::|    |                  \\:::\\____\\");
+    println!("             \\::/____/                 \\:|    |                   \\::/    /");
+    println!("              ~~                        \\|____|                    \\/____/");
+    println!("");
+    println!("v{}", env!("CARGO_PKG_VERSION"));
+    println!("{}", "Running on port: ".to_string() + STATIC_CONFIG.lock().unwrap().get("port").unwrap());
 
     server::set_and_start_server().await;
 }
--- src/param_reader.rs
+++ src/param_reader.rs
@@ -1,5 +1,5 @@
 pub fn read_params(args: Vec<String>) -> String {
-    if !args.is_empty() && args.len() >= 2 {
+    if !args.is_empty() && args.len() >= 3 {
         for i in 1..args.len() {
             if args[i - 1].contains("-properties") {
                 return args[i].to_string();
